a1c2f7e673104d249d3776ab17a52da9f0c99d77
Juan Esteban Echavarria <juanestebanechavarria511@gmail.com>
2025-10-27 19:57:13 -0500

    implementación logica ClasePersona

--- models/personas.py
+++ models/personas.py
@@ -10,37 +10,47 @@ class Persona:
         self.infectada: bool = False
         self.defensa: int = defensa_inicial
         self.infectador: Optional['Persona'] = None
-        self.infectador: Opcional[ 'Persona'] = None
+   
         
     def get_posicion(self) -> tuple[int, int]:
-        ...
+        
+        return (self.x, self.y)
     
     def set_posicion(self, x: int, y:int) -> None:
-        ...
+        self.x = x
+        self.y = y
+       
         
     def esta_infectada(self) -> bool:
-        ...
+        
+        return self.infectada
+    
+    def infectar(self, infectador: 'Persona') -> None:
+        
+        self.infectada = True
+        self.defensa = 0
+        self.infectador = infectador
         
     def reducir_defensa(self) -> None:
-        ...
+        if self.defensa > 0:
+            self.defensa -= 1
         
     def aumentar_defensa(self) -> None:
-        ...
-        
-    def infectar(self, infectador: Persona) -> None:
-        ...
+        self.defensa += 1
         
     def curar(self) -> None:
-        ...
+        self.infectada = False
+        self.defensa = 3
+        self.infectasor = None
         
     def __repr__(self) -> str:
-        ...
+        estado =  "INFECTADA" if self.infectada else "SANA"
+        return f"Persona({self.id}, pos = ({self.x}, {self.y}), def = {self.defensa}, {estado} )"
+
         
     def __eq__(self, other: object) -> bool:
-        ...
-        
+        if not isinstance(other, Persona):
+            return False
+        return self.id == other.id
     
         
-        
- 
-
