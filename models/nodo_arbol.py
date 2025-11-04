dda27c106d5a688220a8791d90ede118a2cdc971
Juan Esteban Echavarria <juanestebanechavarria511@gmail.com>
2025-11-03 22:03:32 -0500

    implementacón clase nodo_arbol

---
+++ models/nodo_arbol.py
@@ -0,0 +1,73 @@
+from typing import Optional
+from persona import Persona
+
+
+class NodoArbol:
+    
+    def __init__(self, persona: Persona) -> None:
+        self.persona: Persona = persona
+        self.padre: Optional['NodoArbol'] = None
+        self.hijos: list['NodoArbol'] = []
+
+    def agregar_hijo(self, nodo_hijo: 'NodoArbol') -> None:
+        ya_existe = False
+        for hijo_actual in self.hijos:
+            if hijo_actual == nodo_hijo:
+                ya_existe = True
+                break
+        
+        es_el_mismo_nodo = (nodo_hijo == self)
+        
+        if not ya_existe and not es_el_mismo_nodo:
+            self.hijos.append(nodo_hijo)
+            nodo_hijo.padre = self
+
+    def eliminar_hijo(self, nodo_hijo: 'NodoArbol') -> bool:
+        hijo_encontrado = False
+        
+        for hijo_actual in self.hijos:
+            if hijo_actual == nodo_hijo:
+                hijo_encontrado = True
+                break
+        
+        if hijo_encontrado:
+            self.hijos.remove(nodo_hijo)
+            nodo_hijo.padre = None
+            return True
+        
+        return False
+
+    def get_hijos(self) -> list['NodoArbol']:
+        return self.hijos
+
+    def get_padre(self) -> Optional['NodoArbol']:
+        return self.padre
+
+    def set_padre(self, nodo_padre: Optional['NodoArbol']) -> None:
+        self.padre = nodo_padre
+
+    def es_raiz(self) -> bool:
+        if self.padre is None:
+            return True
+        else:
+            return False
+
+    def get_nivel(self) -> int:
+        nivel = 0
+        nodo_actual = self
+        
+        while nodo_actual.padre is not None:
+            nivel = nivel + 1
+            nodo_actual = nodo_actual.padre
+        
+        return nivel
+
+    def get_persona(self) -> Persona:
+        return self.persona
+
+    def __repr__(self) -> str:
+        id_persona = self.persona.id
+        texto = f"NodoArbol({id_persona})"
+        return texto
+
+
